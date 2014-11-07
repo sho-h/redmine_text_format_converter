490cb1bbc92495e95cffa51350eb6f04fa95a3b3
Yuya.Nishida <yuya@j96.org>
2014-11-08 02:39:45 +0900

    run "./script/rails generate redmine_plugin redmine_text_format_converter".

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_text_format_converter do
+  name 'Redmine Text Format Converter plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end

599310255a389df64c0136921dd12c60feb2f909
Yuya.Nishida <yuya@j96.org>
2014-11-08 02:58:54 +0900

    write.

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :redmine_text_format_converter do
-  name 'Redmine Text Format Converter plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  name "Redmine Text Format Converter plugin"
+  author "Yuya.Nishida."
+  description "A Redmine plugin for converting test format."
+  version "0.0.0"
+  url "https://github.com/nishidayuya/redmine_text_format_converter"
+  author_url "https://twitter.com/nishidayuya"
 end
